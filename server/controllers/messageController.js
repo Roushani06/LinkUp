d291f64424ece71c312b70d29494b212c6996006
Roushani06 <roushani77100@gmail.com>
2025-10-24 23:09:35 +0530

    Integrate complete backend with frontend(APIs & sockets) for real-time chat functionality

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -76,6 +76,7 @@ export const sendMessage = async(req, res)=>{
         if(image){
             const uploadResponse = await cloudinary.uploader.upload(image)
             imageUrl = uploadResponse.secure_url;
+        }
 
             const newMessage = await Message.create({
                 senderId,
@@ -92,7 +93,7 @@ export const sendMessage = async(req, res)=>{
 
 
             res.json({success: true, newMessage});
-        }
+        
     }catch(error){
     console.log(error.message);
     res.json({success: false, message: error.message})
